c5349619a0feabef19a1c7fc741b3edd856a961d
tyVespA <migliavacca.marco08@gmail.com>
2022-12-22 20:24:17 +0100

    Make menu button accessible
    todo: .darkened transition?

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ const openHamburgerBtn = document.querySelector(".open-mobile-menu");
 const closeHamburgerBtn = document.querySelector(".close-mobile-menu");
 const mobileMenu = document.querySelector(".nav-links");
 const documentBody = document.body;
+let ariaExpandedValue = document.querySelector(".open-mobile-menu").getAttribute("aria-expanded");
 
 openHamburgerBtn.addEventListener("click", () => {
   openHamburgerBtn.classList.add("hidden");
   closeHamburgerBtn.classList.remove("hidden");
   mobileMenu.classList.remove("mobile-shifted-right");
-  documentBody.classList.add("darkened")
+  documentBody.classList.add("darkened");
+
+  if(ariaExpandedValue == "true") {
+    ariaExpandedValue = "false"
+  } else {
+    ariaExpandedValue = "true"
+  }
+  
+  document.querySelector(".open-mobile-menu").setAttribute("aria-expanded", ariaExpandedValue)
 })
 
 closeHamburgerBtn.addEventListener("click", () => {
@@ -15,4 +24,12 @@ closeHamburgerBtn.addEventListener("click", () => {
   closeHamburgerBtn.classList.add("hidden");
   mobileMenu.classList.add("mobile-shifted-right");
   documentBody.classList.remove("darkened")
+
+  if(ariaExpandedValue == "true") {
+    ariaExpandedValue = "false"
+  } else {
+    ariaExpandedValue = "true"
+  }
+  
+  document.querySelector(".open-mobile-menu").setAttribute("aria-expanded", ariaExpandedValue)
 })
